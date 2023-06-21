fa1d8976a7ad55608901b35f568920f847ca5b04
spike04 <rubinbajracharya@yahoo.com>
2023-06-21 14:53:50 +0545

    feat: initial implementation for project

---
+++ pages/course/chapter/[chapterSlug]/lesson/[lessonSlug].vue
@@ -0,0 +1,76 @@
+<script setup>
+const course = useCourse()
+const route = useRoute()
+
+const chapter = computed(() => {
+  return course.chapters.find(
+    (chapter) => chapter.slug === route.params.chapterSlug
+  )
+})
+
+const lesson = computed(() => {
+  return chapter.value.lessons.find(
+    (lesson) => lesson.slug === route.params.lessonSlug
+  )
+})
+
+const title = computed(() => {
+  return `${lesson.value.title} - ${course.title}`
+})
+
+useHead({ title })
+
+const progress = useLocalStorage('progress', [])
+
+const isLessonComplete = computed(() => {
+  if (!progress.value[chapter.value.number - 1]) {
+    return false
+  }
+
+  if (!progress.value[chapter.value.number - 1][lesson.value.number - 1]) {
+    return false
+  }
+
+  return progress.value[chapter.value.number - 1][lesson.value.number - 1]
+})
+
+const toggleComplete = () => {
+  if (!progress.value[chapter.value.number - 1]) {
+    progress.value[chapter.value.number - 1] = []
+  }
+
+  progress.value[chapter.value.number - 1][lesson.value.number - 1] =
+    !isLessonComplete.value
+}
+</script>
+
+<template>
+  <div>
+    <p class="mt-0 uppercase font-bold text-slate-400 mb-1">
+      Lesson {{ chapter.number }} - {{ lesson.number }}
+    </p>
+    <h2 class="my-0">{{ lesson.title }}</h2>
+    <div class="flex space-x-4 mt-2 mb-8">
+      <a
+        v-if="lesson.sourceUrl"
+        class="font-normal text-md text-gray-500"
+        :href="lesson.sourceUrl"
+      >
+        Download Source Code
+      </a>
+      <a
+        v-if="lesson.downloadUrl"
+        class="font-normal text-md text-gray-500"
+        :href="lesson.downloadUrl"
+      >
+        Download Video
+      </a>
+    </div>
+    <VideoPlayer v-if="lesson.videoId" :videoId="lesson.videoId" />
+    <p>{{ lesson.text }}</p>
+    <LessonCompleteButton
+      :model-value="isLessonComplete"
+      @update:model-value="toggleComplete"
+    />
+  </div>
+</template>
