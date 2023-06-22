abed072f6eaae2e59db8c53e30e265146b1047c1
spike04 <rubinbajracharya@yahoo.com>
2023-06-22 12:43:22 +0545

    chor: server side error handling

--- error.vue
+++ error.vue
@@ -1,10 +1,27 @@
+<script setup>
+const error = useError()
+
+const handleError = () => {
+  clearError({
+    redirect:
+      '/course/chapter/1-chapter-1/lesson/1-introduction-to-typescript-with-vue-js-3',
+  })
+}
+</script>
+
 <template>
   <NuxtLayout name="404">
     <div
-      class="p-12 bg-gray-100 w-full h-full min-h-screen flex flex-col items-center space-y-4"
+      class="p-12 bg-gray-100 w-full max-w-full h-full min-h-screen flex flex-col items-center space-y-4"
     >
-      <h1 class="text-4xl font-bold">404 Error</h1>
-      <div class="text-xl">You've Arrived Here on Error, boss</div>
+      <h1 class="text-4xl font-bold">{{ error?.statusCode }}</h1>
+      <div class="text-xl">{{ error?.message }}</div>
+      <p>
+        Go to the
+        <a class="hover:cursor-pointer underline" @click="handleError">
+          First Lesson</a
+        >
+      </p>
     </div>
   </NuxtLayout>
 </template>
