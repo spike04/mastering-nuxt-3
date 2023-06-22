7cfc5aa451a666f4c94278ca58094601123e66c9
spike04 <rubinbajracharya@yahoo.com>
2023-06-22 10:47:37 +0545

    fix: types added to useCourse

--- composables/useCourse.ts
+++ composables/useCourse.ts
@@ -1,14 +1,41 @@
 import courseData from './courseData'
 
-export const useCourse = () => {
+type Course = {
+  title: string
+  chapters: Chapter[]
+}
+
+type Chapter = {
+  title: string
+  slug: string
+  number: number
+  lessons: Lesson[]
+}
+
+type Lesson = {
+  title: string
+  slug: string
+  number: number
+  downloadUrl: string
+  videoId: number
+  text: string
+  sourceUrl?: string
+}
+
+export const useCourse = (): Course => {
+  const chapters: Chapter[] = courseData.chapters.map((chapter) => {
+    const lessons: Lesson[] = chapter.lessons.map((lesson) => ({
+      ...lesson,
+      path: `/course/chapter/${chapter.slug}/lesson/${lesson.slug}`,
+    }))
+    return {
+      ...chapter,
+      lessons,
+    }
+  })
+
   return {
     ...courseData,
-    chapters: courseData.chapters.map((chapter) => ({
-      ...chapter,
-      lessons: chapter.lessons.map((lesson) => ({
-        ...lesson,
-        path: `/course/chapter/${chapter.slug}/lesson/${lesson.slug}`,
-      })),
-    })),
+    chapters,
   }
 }
